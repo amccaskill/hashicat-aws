1bb3c43574cc459dba5048f9bd0bd9500500808d
adrian <mccaskill.adrian@gmail.com>
2021-09-17 00:03:40 +0000

    added the second tage

--- main.tf
+++ main.tf
@@ -132,7 +132,8 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "devops"
+    Department = Billable
+    value = "true"
   }
 }
 
